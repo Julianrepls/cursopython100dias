19c6262e13363f8f19b9828ad38e4d8e28d943df
Julian Espadas <j.espadaslinares@gmail.com>
2024-04-11 11:33:06 +0200

    Adding day 32 learning about lists, excercise returning a random item from the list

---
+++ day32_lists.py
@@ -0,0 +1,38 @@
+timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+print(timetable[0])
+print(timetable[1])
+print(timetable[2])
+print(timetable[3])
+print(timetable[4])
+
+timetable[4] = "Watch TV"
+
+print(timetable)
+
+# timetable[4]= "Watch TV"
+# print(timetable[0])
+# print(timetable[1])
+# print(timetable[2])
+# print(timetable[3])
+# print(timetable[4])
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Watch TV"]
+# for lesson in timetable:
+#   print(lesson)
+
+
+# exercise 1: print a random objet from the list
+import random
+
+timetable = ["Computer Science", "Math", "English", "Art", "Watch TV"]
+index = random.randint(0,5)
+print(timetable[index])
+
+# import random
+# greetings = ["Hello there!", "Konnichiwa", "Guten Tag!", "Bore Da!"]
+# index = random.randint(0,3)
+# print(greetings[index])
+
+
+
+# </details>
